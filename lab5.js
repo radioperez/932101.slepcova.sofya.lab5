0a899d820f38a85ff7accc3abd3361dc573ed80a
Slepcova <brostikslavik@yandex.com>
2023-10-11 19:47:08 +0700

    Finished:)

---
+++ lab5.js
@@ -0,0 +1,22 @@
+function displayPost1() {
+	document.getElementById("whitecover").style.display = "block";
+	document.getElementById("f1").style.display = "block";
+}
+
+function displayPost2() {
+	document.getElementById("whitecover").style.display = "block";
+	document.getElementById("f2").style.display = "block";
+}
+
+function displayPost3() {
+	document.getElementById("whitecover").style.display = "block";
+	document.getElementById("f3").style.display = "block";
+}
+
+function unDisplay() {
+	document.getElementById("whitecover").style.display = "none";
+	document.getElementById("f1").style.display = "none";
+	document.getElementById("f2").style.display = "none";
+	document.getElementById("f3").style.display = "none";
+}
+
